a91284b783646d0f2b32047b9929bb3cde5e6086
felipedevpips-max <felipedevpips@gmail.com>
2026-01-07 13:02:46 -0300

    instegrants

--- src/components/Carrossel/index.jsx
+++ src/components/Carrossel/index.jsx
@@ -2,48 +2,54 @@ import { useState } from "react";
 import styles from "./carrossel.module.css";
 import sagaz from "../../assets/sagaz.jpg";
 import maicon from "../../assets/maicon.jpg";
+import felipe from "../../assets/felipe.jpg";
+import jonathan from "../../assets/jonathan.jpg";
+import luiz from "../../assets/luiz.jpg";
+import instagram from "../../assets/instagram.svg";
+
+
 
 
 const pessoas = [
   {
     id: 1,
-    nome: "David/Sagaz",
-    nascimento: "10/05/1990",
-    contato: "joao@email.com",
-    posicao:"",
-    imagem: sagaz
+    nome: "David Luiz 'Sagaz'",
+    nascimento: "11/02/1998",
+    contato: instagram,
+    posicao: "Vocalista/Violão",
+    imagem: sagaz,
   },
   {
     id: 2,
-    nome: "Maicon Vinicius/Faráo",
-    nascimento: "22/08/1995",
-    contato: "maria@email.com",
-    posicao:"",
-    imagem: maicon
+    nome: "Maicon Vinicius 'Faráo'",
+    nascimento: "07/10/1998",
+    contato: instagram,
+    posicao: "Vocalista/Tecladista",
+    imagem: maicon,
   },
   {
     id: 3,
-    nome: "Carlos Lima",
-    nascimento: "01/12/1988",
-    contato: "carlos@email.com",
-    posicao:"",
-    imagem: "https://via.placeholder.com/200"
+    nome: "Luiz Junior",
+    nascimento: "12/09/2000",
+    contato: instagram,
+    posicao: "",
+    imagem: luiz,
   },
   {
     id: 3,
-    nome: "Carlos Lima",
-    nascimento: "01/12/1988",
-    contato: "carlos@email.com",
-    posicao:"",
-    imagem: "https://via.placeholder.com/200"
+    nome: "Jonathan Luiz",
+    nascimento: "02/07/1994",
+    contato: instagram,
+    posicao: "",
+    imagem: jonathan,
   },
   {
     id: 3,
     nome: "Felipe Costa",
-    nascimento: "01/12/1988",
-    contato: "carlos@email.com",
-    posicao:"Desenvolvedor Web/ Guitarrista",
-    imagem: "https://via.placeholder.com/200"
+    nascimento: "30/12/2000",
+    contato: instagram,
+    posicao: "Desenvolvedor Web/ Guitarrista",
+    imagem: felipe,
   },
 ];
 
@@ -62,25 +68,35 @@ export default function Carrossel() {
 
   return (
     <>
-    <section className={styles.integrants}>
-    <h2>Integrantes</h2>
-    <div className={styles.carrossel}>
-      <button onClick={anterior}>◀</button>
+      <section className={styles.integrants}>
+        <h2>Integrantes</h2>
+        <div className={styles.carrossel}>
+          <button onClick={anterior}>◀</button>
 
-      <div className={styles.card}>
-        <img src={pessoa.imagem} alt={pessoa.nome} />
+          <div className={styles.card}>
+            <img src={pessoa.imagem} alt={pessoa.nome} />
 
-        <div className={styles.info}>
-          <h2>{pessoa.nome}</h2>
-          <p><strong>Data de nascimento:</strong> {pessoa.nascimento}</p>
-          <p><strong>Posição:</strong> {pessoa.posicao}</p>
-          <p><strong>Contato:</strong> {pessoa.contato}</p>
+            <div className={styles.info}>
+              <h2>{pessoa.nome}</h2>
+              <p>
+                <strong>Data de nascimento:</strong> {pessoa.nascimento}
+              </p>
+              <p>
+                <strong>Posição:</strong> {pessoa.posicao}
+              </p>
+              <p>
+                <strong>Contato:</strong> 
+                <img src={pessoa.contato}
+                alt="Contato"
+                className={styles.icon}/>
+                
+              </p>
+            </div>
+          </div>
+
+          <button onClick={proximo}>▶</button>
         </div>
-      </div>
-
-      <button onClick={proximo}>▶</button>
-    </div>
-    </section>
+      </section>
     </>
   );
 }

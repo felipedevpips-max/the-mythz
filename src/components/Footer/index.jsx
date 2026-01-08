9cea551c13498b3746f23ab67c5e98456d68e34b
felipedevpips-max <felipedevpips@gmail.com>
2026-01-08 18:28:07 -0300

    aplicando logo no footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,26 +1,32 @@
 import styles from "./footer.module.css";
+import logo from "../../assets/logo.png";
 
 export const Footer = () => {
   return (
     <>
-    
-    <footer className={styles.footer}>
-    <hr className={styles.hr}/>
-        
-        <div className={styles.contato}>
-            
-        </div>
+      <footer className={styles.footer}>
+        <hr className={styles.hr} />
+        <div className={styles.contain}>
+          <div className={styles.contato}>
+            <a href="/" onClick={() => window.location.reload()}>
+              <img className={styles.img} src={logo} alt="" />
+            </a>
+            <ul>
+              <li><a></a></li>
+              <li><a></a></li>
+              <li><a></a></li>
+              <li><a></a></li>
+              <li><a></a></li>
+            </ul>
+          </div>
 
+          <div className={styles.container}>
+            <p>© 2026 • The MYTHZ</p>
 
-
-
-
-        <div className={styles.container}>
-          <p>© 2026 • The MYTHZ</p>
-
-          <nav>
-            <a href="#home">Home</a>
-          </nav>
+            <nav>
+              <a href="#home">Home</a>
+            </nav>
+          </div>
         </div>
       </footer>
     </>
